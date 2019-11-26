18dfdb99659a4ffee6b9f832972fc424ded1c3f6
Esteban <montesteban0@gmail.com>
2019-11-26 13:01:12 -0600

    agregamos a mi mascota con un append

---
+++ 02,array append.py
@@ -0,0 +1,9 @@
+def main():
+    familia = ["Daniel", "Adriana", "Abraham", "Maritza", "Claudia", "Esteban"]
+    print(familia)
+    familia.append("Flash")
+    print(familia)
+
+
+if __name__ == '__main__':
+    main()
