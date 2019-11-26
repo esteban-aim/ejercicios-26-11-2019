53c1a88ea39414581302e2bf37660847e39b9a83
Esteban <montesteban0@gmail.com>
2019-11-26 13:11:39 -0600

    use el array index para encontar una posision para que me dijera un nombre

---
+++ 03 array index.py
@@ -0,0 +1,9 @@
+def main():
+    familia = ["Daniel","Adriana","Abraham","Maritza","Claudia","Esteban"]
+    posision = familia.index("Esteban")
+    print(posision)
+    print(familia[posision])
+
+
+if __name__ == "__main__":
+    main()
